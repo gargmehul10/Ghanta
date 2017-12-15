c170f44564d22c716f1d4415895ecb72b9fea8b8
Mehul Garg <garg.mehul10@gmail.com>
2017-12-15 18:48:16 +0530

    Create DataModel.java

---
+++ Ghanta/app/src/main/java/com/mehulgarg/ghanta/DataModel.java
@@ -0,0 +1,19 @@
+package com.mehulgarg.ghanta;
+
+/**
+ * Created by Mehul Garg on 09-10-2017.
+ */
+
+public class DataModel {
+
+    String time;
+
+    public DataModel(String time) {
+
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
